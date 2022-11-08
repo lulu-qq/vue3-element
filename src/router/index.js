c41db29fee5dfcd13c7681f19e444a7046b45263
ll <17865358153@163.com>
2022-11-08 17:05:57 +0800

    vue create

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/Index.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="box">
+        <el-row>
+            <el-col :span="24">
+                <div class="grid-content ep-bg-purple-dark" >
+                    <el-card class="box-card" >
+                        <template #header>
+                        <div class="card-header">
+                            <span>系统登陆</span>
+                            <el-icon class="icons"><Plus /></el-icon>
+                        </div>
+                        </template>
+                        <el-form :model="form">
+                            <el-form-item>
+                                <el-input 
+                                    v-model="form.name"  
+                                    :prefix-icon="UserFilled"
+                                />
+                            </el-form-item>
+                            <el-form-item>
+                                <el-input 
+                                    type="password" 
+                                    v-model="form.password"  
+                                    :prefix-icon="Hide"
+                                    show-password
+                                    />
+                            </el-form-item>
+                            <el-form-item>
+                                <el-button 
+                                  type="primary" 
+                                  @click="onSubmit"
+                                  class="subButton"
+                                >登录</el-button>
+                            
+                            </el-form-item>
+                            <el-form-item
+                                class="add"
+                            >
+                                <el-button 
+                                  type="primary" 
+                                  @click="onSubmit"
+                                >第三方登录</el-button>
+                            </el-form-item>
+                        </el-form>
+                    </el-card>
+                </div>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+import { UserFilled, Hide, Plus } from "@element-plus/icons-vue"
+// do not use same name with ref
+const form = reactive({
+  name: '',
+  password:''
+})
+
+const onSubmit = () => {
+  console.log('submit!')
+}
+</script>
+
+
+<style lang="scss" scoped>
+.box{
+    height:100vh;
+    width: 100%;
+    background:skyblue;
+    .card-header{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position:relative;
+        .icons{
+            position:absolute !important;
+            right:0;
+            top:-2px;
+        }
+    }
+    .grid-content{
+        display: flex;
+        justify-content: center;
+    }
+    .el-row{
+        height:100%;
+        align-items: center;
+        justify-content: center;
+        .box-card{
+            width:520px;
+        }
+    }
+    .subButton{
+        width: 100%;
+    } 
+}
+</style>
+<style lang="scss">
+  .add{
+         .el-form-item__content{
+            justify-content: end !important;
+        }
+    } 
+    
+</style>
